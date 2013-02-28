ed8d7e5239d68ab469826e2bccd5b4444cd51ef1
Steve Erikson <steve.erikson.psk@gmail.com>
2013-02-28 02:51:36 -0500

    seriously we need to have consistent styles between all of us. we should use NORMAL tabs (8 spaces) for comaptibility with all editors. if and for are not functions, they are keywoards. you need to do 'if (cond)' for the same reason you do 'return (cond)'. also there should be spaces on both sides of operators and }else{ should be split up into new lines--this is all for easier readability. i created main.cpp a long time ago to give an example of style. if we need to ALL have consistent style in order to pass this class then we should be doing it the CORRECT way. sorry if this seems picky, i just know this is a requirement and if we all write the same way i won't need to update it later...

--- src/map.cpp
+++ src/map.cpp
@@ -8,62 +8,67 @@ Map::Map(int x, int y)
         map_x = x;
         map_y = y;
         map = new tile_pair * [x];
-        for(int i=0;i<x;i++)
+        for (int i = 0; i < x; i++)
         {
-            map[i] = new tile_pair [y];
-            for(int j=0;j<y;j++)
-            {
-                map[i][j].unit = NULL;
-                map[i][j].terrain = map[i][j].terrain % 6 + 1;//temp
-            }
+		map[i] = new tile_pair [y];
+		for (int j = 0; j < y; j++)
+		{
+			map[i][j].unit = NULL;
+			map[i][j].terrain = map[i][j].terrain % 6 + 1;	//temp
+		}
         }
-        for(int i=0; i<scr_x; i++)
+	for (int i = 0; i < scr_x; i++)
         {
-            for(int j=0;j<scr_y; j++)
-            {
-                if(i >= x || j >= y)
-                {
-                    attron(COLOR_PAIR(14));
-                    mvaddch(j,i,' ');
-                    attroff(COLOR_PAIR(14));
-                }else{
-                    if(map[i][j].unit == NULL)
-                    {
-                        attron(COLOR_PAIR(map[i][j].terrain));
-                        mvaddch(j,i,' ');
-                        attroff(COLOR_PAIR(map[i][j].terrain));
-                    }
-                }
-            }
+		for(int j=0;j<scr_y; j++)
+		{
+			if (i >= x || j >= y)
+			{
+				attron(COLOR_PAIR(14));
+				mvaddch(j,i,' ');
+				attroff(COLOR_PAIR(14));
+			}
+			else
+			{
+				if (map[i][j].unit == NULL)
+                    		{
+					attron(COLOR_PAIR(map[i][j].terrain));
+					mvaddch(j,i,' ');
+					attroff(COLOR_PAIR(map[i][j].terrain));
+				}
+			}
+		}
         }
 }
 
 Map::~Map()
 {
-    for(int i=0;i<map_x;i++)
-        delete [] map[i];
+	for(int i = 0; i < map_x; i++)
+		delete [] map[i];
 }
 
 void Map::redraw()
 {
-    getmaxyx(stdscr,scr_y,scr_x);
-    for(int i=0;i<scr_x;i++)
-    {
-        for(int j=0;j<scr_y;j++)
-        {
-            if(i+tile_x >= map_x || j+tile_y >= map_y)
-            {
-                attron(COLOR_PAIR(14));
-                mvaddch(j,i,' ');
-                attroff(COLOR_PAIR(14));
-            }else{
-                if(map[i+tile_x][j+tile_y].unit == NULL)
-                {
-                    attron(COLOR_PAIR(map[i+tile_x][j+tile_y].terrain));
-                    mvaddch(j,i,' ');
-                    attroff(COLOR_PAIR(map[i+tile_x][j+tile_y].terrain));
-                }
-            }
-        }
-    }
+	getmaxyx(stdscr,scr_y,scr_x);
+	for (int i = 0; i < scr_x; i++)
+	{
+		for (int j = 0; j < scr_y; j++)
+		{
+			if (i + tile_x >= map_x || j + tile_y >= map_y)
+			{
+				attron(COLOR_PAIR(14));
+				mvaddch(j,i,' ');
+				attroff(COLOR_PAIR(14));
+			}
+
+			else
+			{
+				if (map[i + tile_x][j + tile_y].unit == NULL)
+				{                
+					attron(COLOR_PAIR(map[i + tile_x][j + tile_y].terrain));
+					mvaddch(j,i,' ');
+					attroff(COLOR_PAIR(map[i + tile_x][j + tile_y].terrain));
+				}
+			}
+		}
+	}
 }
