e2d97c9eb0c0b3c8af2acde079a203023f4c12bf
stevepsk <erikss@rpi.edu>
2013-03-31 22:16:18 -0400

    got rid of new/delete to prevent leaks and to use RAII style

--- include/game.h
+++ include/game.h
@@ -2,21 +2,22 @@
  * File:  game.h
  * Authors:  Stephen Erikson, Michael Pomeranz, James Lenze, Kelly DeBarr
  * Date:  28 March 2013
- * Description:  Game class definition
+ * Description:  Game class declaration file
  */
 
 #ifndef GAME_H_
 #define GAME_H_
 
+#include <vector>
 #include "player.h"
 
 class Game
 {
-	Player *players[2];
+	const int num_players = 2;
+	std::vector<Player> players;
 	
 public:
 	Game();
-	~Game();
 };
 
 #endif
