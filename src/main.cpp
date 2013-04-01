d03fb0913653da8faba96adbd95fd6d9241490be
stevepsk <erikss@rpi.edu>
2013-04-01 04:38:44 -0400

    main starts up the menu now

--- src/main.cpp
+++ src/main.cpp
@@ -8,50 +8,57 @@
 #include <ncurses/curses.h>
 #include <iostream>
 #include <signal.h>
+#include "menu.h"
 #include "map.h"
 #include "panel.h"
 #include "unit.h"
 
 int main()
 {
-    /*START NCURSES*/
-    initscr();
-    noecho();
-    raw();
-    refresh();
-    keypad(stdscr,TRUE);
-    /*START COLORS*/
-    start_color();
-    /*player 1 colors*/
-    init_pair(1, COLOR_WHITE, COLOR_BLUE);
-    init_pair(2, COLOR_WHITE, COLOR_GREEN);
-    init_pair(3, COLOR_WHITE, COLOR_CYAN);
-    init_pair(4, COLOR_WHITE, COLOR_RED);
-    init_pair(5, COLOR_WHITE, COLOR_YELLOW);
-    init_pair(6, COLOR_WHITE, COLOR_MAGENTA);
-    /*player 2 colors*/
-    init_pair(7, COLOR_BLACK, COLOR_BLUE);
-    init_pair(8, COLOR_BLACK, COLOR_GREEN);
-    init_pair(9, COLOR_BLACK, COLOR_CYAN);
-    init_pair(10, COLOR_BLACK, COLOR_RED);
-    init_pair(11, COLOR_BLACK, COLOR_YELLOW);
-    init_pair(12, COLOR_BLACK, COLOR_MAGENTA);
-    /*error colors*/
-    init_pair(13, COLOR_RED, COLOR_WHITE);
-    init_pair(14, COLOR_BLACK, COLOR_BLACK);
-    /*panel color*/
-    init_pair(15, COLOR_WHITE, COLOR_BLACK);
-    /*RESIZE FUNC*/
-    //signal(SIGWINCH, /*i(void *)resizefunc(int)*/);
-    int scr_x, scr_y;
-    getmaxyx(stdscr,scr_y,scr_x);
-    Panel *panel = new Panel(scr_x,scr_y,20);
-    Map *gamemap = new Map(200,200,scr_x,scr_y,panel);
-    gamemap->map_loop();
-    delete gamemap;
-    delete panel;
+	Menu menu;
+	menu.show();
 
-    /*end ncurses*/
-    endwin();
+/*
+	// START NCURSES
+	initscr();
+	noecho();
+	raw();
+	refresh();
+	keypad(stdscr,TRUE);
+	// START COLORS
+	start_color();
+	// player 1 colors
+	init_pair(1, COLOR_WHITE, COLOR_BLUE);
+	init_pair(2, COLOR_WHITE, COLOR_GREEN);
+	init_pair(3, COLOR_WHITE, COLOR_CYAN);
+	init_pair(4, COLOR_WHITE, COLOR_RED);
+	init_pair(5, COLOR_WHITE, COLOR_YELLOW);
+	init_pair(6, COLOR_WHITE, COLOR_MAGENTA);
+	// player 2 colors
+	init_pair(7, COLOR_BLACK, COLOR_BLUE);
+	init_pair(8, COLOR_BLACK, COLOR_GREEN);
+	init_pair(9, COLOR_BLACK, COLOR_CYAN);
+	init_pair(10, COLOR_BLACK, COLOR_RED);
+	init_pair(11, COLOR_BLACK, COLOR_YELLOW);
+	init_pair(12, COLOR_BLACK, COLOR_MAGENTA);
+	// error colors
+	init_pair(13, COLOR_RED, COLOR_WHITE);
+	init_pair(14, COLOR_BLACK, COLOR_BLACK);
+	// panel color
+	init_pair(15, COLOR_WHITE, COLOR_BLACK);
+	// RESIZE FUNC
+	//signal(SIGWINCH, );//i(void *)resizefunc(int));
+	int scr_x, scr_y;
+	getmaxyx(stdscr,scr_y,scr_x);
+	Panel *panel = new Panel(scr_x,scr_y,20);
+	Map *gamemap = new Map(200,200,scr_x,scr_y,panel);
+	gamemap->map_loop();
+	delete gamemap;
+	delete panel;
+	
+	// end ncurses
+	endwin();
+*/
+
 	return 0;
 }
