94fef685f4ff04edbb65500f9a8a39cd3c4071e4
stevepsk <steve.erikson.psk@gmail.com>
2013-05-06 07:39:36 -0400

    mostly style updates, also making accessors const

--- include/player.h
+++ include/player.h
@@ -1,39 +1,40 @@
 /*
  * File:  player.h
- * Authors:  Stephen Erikson, Michael Pomeranz, James Lenze, Kelly DeBarr
+ * Authors:  Stephen Erikson, Michael Pomeranz, Kelly DeBarr
  * Date:  28 March 2013
- * Description:  Player class declaration file
+ * Description:  Player class declaration file.
  */
 
-#ifndef PLAYER_H_
-#define PLAYER_H_
+#ifndef PLAYER_H
+#define PLAYER_H
 
 #include <list>
 #include <vector>
-#include "unit.h"
 
-//Struct to hold locations
-struct Location
+class Unit;
+
+/* Struct to hold locations. */
+struct location_t
 {
-    int x;
-    int y;
+	int x;
+	int y;
 };
 
-//Player class, holds data for each player
+/* Player class, holds data for each player. */
 class Player
 {
-    int id;
+	int id;
 	std::list<Unit *> units;
-    std::vector<Location> bases;
+	std::vector<location_t> bases;
 
 public:
 	Player(int i) : id(i), units(), bases() { }
-    int get_id() { return id; }
-    std::list<Unit *>::iterator add_unit(Unit *);
+	int get_id() const { return id; }
+	std::vector<location_t> const &get_bases() const { return bases; }
+	std::list<Unit *>::iterator add_unit(Unit *);
 	void del_unit(std::list<Unit *>::iterator);
-    void reset_units();
-    void add_base(int x, int y);
-    std::vector<Location> &get_bases() { return bases; }
+	void reset_units();
+	void add_base(int x, int y);
 };
 
-#endif
+#endif	/* PLAYER_H */
--- src/player.cpp
+++ src/player.cpp
@@ -1,38 +1,43 @@
 /*
  * File:  player.cpp
- * Authors:  Stephen Erikson, Michael Pomeranz, James Lenze, Kelly DeBarr
+ * Authors:  Stephen Erikson, Michael Pomeranz, Kelly DeBarr
  * Date:  28 March 2013
- * Description:  Player class implementation file
+ * Description:  Player class definition file.
  */
 
 #include "player.h"
 
-//adds a unit to the players list of current units
+#include "unit.h"
+
+/* Adds a unit to the players list of current units. */
 std::list<Unit *>::iterator Player::add_unit(Unit *unit)
 {
-	this->units.push_front(unit);
-    return this->units.begin();
+	units.push_front(unit);
+	return units.begin();
 }
 
-//removes a unit from the players list of current units
+/* Removes a unit from the players list of current units. */
 void Player::del_unit(std::list<Unit *>::iterator it)
 {
-	this->units.erase(it);
+	units.erase(it);
 }
 
-//resets the used flag on all units that player controls
-//used at the start of a turn to reset units
+/*
+ * Resets the used flag on all units that player controls.
+ * Used at the start of a turn to reset units.
+ */
 void Player::reset_units()
 {
-    for(std::list<Unit *>::iterator itr=units.begin();itr!=units.end();itr++)
-        (*itr)->set_used(false);
+	using std::list;
+	for(list<Unit *>::iterator itr = units.begin(); itr != units.end(); ++itr)
+		(*itr)->set_used(false);
 }
 
-//adds the location of a base to the list
+/* Adds the location of a base to the list. */
 void Player::add_base(int x, int y)
 {
-    Location temp;
-    temp.x = x;
-    temp.y = y;
-    bases.push_back(temp);
+	location_t temp;
+	temp.x = x;
+	temp.y = y;
+	bases.push_back(temp);
 }
