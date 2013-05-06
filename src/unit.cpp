932c2eb8bce3c576159d12e2769451340c9e0763
stevepsk <steve.erikson.psk@gmail.com>
2013-05-06 08:12:40 -0400

    switched panel to ptr

--- src/unit.cpp
+++ src/unit.cpp
@@ -1,49 +1,53 @@
 /*
  * File:  unit.cpp
- * Authors:  Steve Erikson, Michael, James, Kelly
+ * Authors:  Steve Erikson, Michael Pomeranz, Kelly DeBarr
  * Date:  14 February 2013
- * Description:  Parent unit class implementation file.
+ * Description:  Parent unit class definition file.
  */
 
 #include "unit.h"
+
+#include <cstdlib>
 #include "player.h"
 
-//Unit constructor, adds the unit to the players list of units
+/* Unit constructor, adds the unit to the players list of units. */
 Unit::Unit(UnitType *unit_type, Player *play, int x, int y) :
-    type(unit_type),
-    player(play),
-    used(false),
-    place(),
-    mapx(x),
-    mapy(y)
+	type(unit_type),
+	player(play),
+	used(false),
+	place(),
+	mapx(x),
+	mapy(y)
 {
-    if(play != 0)
-        place = play->add_unit(this);
+	if (play != 0)
+		place = play->add_unit(this);
 }
 
-//Unit deconstructor, removes the unit from the players list of units
+/* Unit deconstructor, removes the unit from the players list of units. */
 Unit::~Unit()
 {
-    player->del_unit(place);
+	player->del_unit(place);
 }
 
-//returns the cost of moving over a specific terrain
-int Unit::terrain_cost(int terrain)
+/* Returns the cost of moving over a specific terrain. */
+int Unit::terrain_cost(int terrain) const
 {
-    return type->get_terrain_cost()[terrain-1];
+	return type->get_terrain_cost()[terrain-1];
 }
 
-//This unit attacks a unit of a unit type based off the label
-//If this unit kills the other unit, 0 is returned
-//If both units die, 1 is returned
-//If only this unit dies, 2 is returned
+/*
+ * This unit attacks a unit of a unit type based off the label.
+ * If this unit kills the other unit, 0 is returned.
+ * If both units die, 1 is returned.
+ * If only this unit dies, 2 is returned.
+ */
 int Unit::attack(int label)
 {
-    int num = std::rand() % 10 + 1;
-    int *ptr = type->get_unit_attack();
-    if(num <= ptr[label*2])
+	int num = std::rand() % 10 + 1;
+	int *ptr = type->get_unit_attack();
+    if (num <= ptr[label*2])
         return 0;
-    if(num <= ptr[label*2+1])
+    if (num <= ptr[label*2+1])
         return 1;
     return 2;
 }
