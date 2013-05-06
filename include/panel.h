18da77d9555f1d651bbc1e6c659cc0f2f52e0e82
stevepsk <steve.erikson.psk@gmail.com>
2013-05-06 06:52:52 -0400

    style updates & changing unecessary include to fwd declaration

--- include/panel.h
+++ include/panel.h
@@ -1,30 +1,38 @@
+/*
+ * File:  panel.h
+ * Authors:  Stephen Erikson, Michael Pomeranz, Kelly DeBarr
+ * Date:  16 March 2013
+ * Description:  Panel class definition file.
+ */
+
 #ifndef PANEL_H
 #define PANEL_H
 
 #include <curses.h>
-#include "unit.h"
 
-//Class that creates an information panel on the screen
+class Unit;
+
+/* Class that creates an information panel on the screen. */
 class Panel
 {
-    int scr_x, scr_y;//total size of screen
-    int size;//panel width
-    int pos_x;//panel starting x position
-    int terrain_pos;//line with terrain info
-    int unit_name_pos;//line with unit name
-    int move_pos;//line with unit move info
-    int selected_unit_pos;
-    WINDOW *panel_window;
-
-    //unsued constructors to stop warnings
-    void operator=(const Panel &p);
-
-    public:
-    Panel(int screen_x, int screen_y, int panel_size);
-    ~Panel();
-    void update(int terrain, Unit *unit, Unit *selected, int path_length);
-    void resize(int screen_x, int screen_y, int terrain, Unit *unit, Unit *selected, int path);
-    int get_size();
+	int scr_x, scr_y;	// Total size of screen
+	int size	;	// Panel width
+	int pos_x;		// Panel starting x position
+	int terrain_pos;	// Line with terrain info
+	int unit_name_pos; 	// Line with unit name
+	int move_pos;		// Line with unit move info
+	int selected_unit_pos;
+	WINDOW *panel_window;
+	
+	// Unsued constructors to stop warnings
+	void operator=(const Panel &p);
+	
+public:
+	Panel(int screen_x, int screen_y, int panel_size);
+	~Panel();
+	void update(int terrain, Unit *unit, Unit *selected, int path_length);
+	void resize(int screen_x, int screen_y, int terrain, Unit *unit, Unit *selected, int path);
+	int get_size() const { return size; }	// Returns width of panel
 };
 
 #endif
