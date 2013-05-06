d82c1359b7c1f48197b2105f881e936973eeb0ae
stevepsk <steve.erikson.psk@gmail.com>
2013-05-06 03:42:04 -0400

    style updates & changing unnecessary includes to forward declarations

--- include/build_panel.h
+++ include/build_panel.h
@@ -1,39 +1,40 @@
 /*
  * File:  build_panel.h
- * Authors:  Stephen Erikson, Michael Pomeranz, James Lenze, Kelly DeBarr
+ * Authors:  Stephen Erikson, Michael Pomeranz, Kelly DeBarr
  * Date:  11 March 2013
- * Description: BuildPanel class declaration file
+ * Description:  BuildPanel class declaration file.
  */
 
-#ifndef BUILD_PANEL_H_
-#define BUILD_PANEL_H_
+#ifndef BUILD_PANEL_H
+#define BUILD_PANEL_H
 
-#include <curses.h>
 #include <vector>
 #include <cstdlib>
-#include "player.h"
-#include "unit.h"
-#include "unit_type.h"
+#include <curses.h>
 
-//Class used to create a menu to build new units
+class Player;
+class Unit;
+class UnitType;
+
+/* Class used to create a menu to build new units. */
 class BuildPanel
 {
-    std::vector<UnitType *> types;
+	std::vector<UnitType *> types;
 	WINDOW *win;
 	Player *pl;
-    int size;
-    unsigned int opt;
-    int screen_y;
+	int size;
+	unsigned int opt;
+	int screen_y;
 
-    //unsued constructors (added to prevent warnings)
-    BuildPanel(const BuildPanel &b); 
-    void operator=(const BuildPanel &b);
+	// Unsued constructors (added to prevent warnings)
+	BuildPanel(const BuildPanel &b); 
+	void operator=(const BuildPanel &b);
 
 public:
-	BuildPanel(std::vector<UnitType *> &unit_types, Player *play, int scr_x, int scr_y, int panel_size);
-    ~BuildPanel();
-    void resize(int scr_x, int scr_y);
+	BuildPanel(std::vector<UnitType *> const &unit_types, Player *play, int scr_x, int scr_y, int panel_size);
+	~BuildPanel();
+	void resize(int scr_x, int scr_y);
 	Unit *use_panel(int x, int y);
 };
 
-#endif
+#endif	/* BUILD_PANEL_H */
